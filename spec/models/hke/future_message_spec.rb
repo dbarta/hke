b5f45f16b1b9fe7c17042808fdfc182bed39091c
dbarta <david@odeca.net>
2024-08-27 17:17:07 +0300

    fixed model spec

--- spec/models/hke/future_message_spec.rb
+++ spec/models/hke/future_message_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 module Hke
   RSpec.describe FutureMessage, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    # pending "add some examples to (or delete) #{__FILE__}"
   end
 end
