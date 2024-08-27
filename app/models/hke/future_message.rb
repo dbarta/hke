c63c11a42ebe79da2efa1426b0c7ded6f5aa1e9d
dbarta <david@odeca.net>
2024-08-27 17:12:16 +0300

    debug validations

--- app/models/hke/future_message.rb
+++ app/models/hke/future_message.rb
@@ -10,10 +10,10 @@ module Hke
 
     # Validations
     # validates :send_date, presence: true, timeliness: {on_or_after: -> { Time.current }, type: :datetime}
-    validates :delivery_method, presence: true
-    validates :full_message, presence: true
-    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, if: -> { email.present? }
-    validates :phone, phone: true, if: -> { phone.present? }
+    # validates :delivery_method, presence: true
+    # validates :full_message, presence: true
+    # validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, if: -> { email.present? }
+    # validates :phone, phone: true, if: -> { phone.present? }
 
     # Placeholder for message_type validation; clarify what message_type represents
     # validates :message_type, presence: true, inclusion: { in: [/* expected values */] }
