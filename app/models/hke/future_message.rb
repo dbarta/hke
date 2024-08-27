62f8d3aa7e6a27713ef14c38bd97de1322e88100
dbarta <david@odeca.net>
2024-08-27 17:11:06 +0300

    fix timeliness

--- app/models/hke/future_message.rb
+++ app/models/hke/future_message.rb
@@ -9,7 +9,7 @@ module Hke
     enum delivery_method: {email: 0, phone: 1, both: 2}
 
     # Validations
-    validates :send_date, presence: true, timeliness: {on_or_after: -> { Time.current }, type: :datetime}
+    # validates :send_date, presence: true, timeliness: {on_or_after: -> { Time.current }, type: :datetime}
     validates :delivery_method, presence: true
     validates :full_message, presence: true
     validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, if: -> { email.present? }
