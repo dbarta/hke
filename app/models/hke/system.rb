979d4aa97971663e27ab138d1becef1f42e9af69
dbarta <david@odeca.net>
2024-09-03 20:52:59 +0300

    Added include of concern into System

--- app/models/hke/system.rb
+++ app/models/hke/system.rb
@@ -1,5 +1,6 @@
 module Hke
   class System < ApplicationRecord
+    include Hke::SystemInfoConcern
     has_one :preference, as: :preferring, class_name: "Hke::Preference", dependent: :destroy
 
     # Singleton pattern to ensure only one instance of System
