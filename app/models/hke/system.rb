87fc40c3c3ccc092679078d74780ced03e8c6cc7
dbarta <david@odeca.net>
2024-09-04 13:07:05 +0300

    debug

--- app/models/hke/system.rb
+++ app/models/hke/system.rb
@@ -6,7 +6,8 @@ module Hke
     # Singleton pattern to ensure only one instance of System
     def self.instance
       i = first_or_create
-      puts "@@@@ after first_or_create, preference: #{i.preference}"
+      p = i.preference
+      puts "@@@@ after first_or_create, preference: #{p} attributes:#{p.attributes}"
       i
     end
 
