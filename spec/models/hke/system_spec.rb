47d34d1b2cafb3204b5493136b2a2c0fc6695c0a
dbarta <david@odeca.net>
2024-09-04 14:46:44 +0300

    added a test

--- spec/models/hke/system_spec.rb
+++ spec/models/hke/system_spec.rb
@@ -55,5 +55,34 @@ RSpec.describe Hke::System, type: :model do
         expect(preferences["how_many_days_before_yahrzeit_to_send_message"]).to eq([2, 4, 6])
       end
     end
+
+    context "when updating the preference of the system" do
+      it "returns the correct updated preferences" do
+        # Create a System record
+        system = Hke::System.instance
+        system.update(product_name: "Test Product 2", version: "2.0.0")
+
+        # Create and associate a new Preference record with the System
+        system.update_preference(
+          enable_send_email: true,
+          enable_send_sms: true,
+          how_many_days_before_yahrzeit_to_send_message: [3, 4, 5]
+        )
+
+        # Clear the cache and fetch updated system info
+        Hke::System.clear_system_info_cache
+        info = system_info
+
+        # Assertions for system info
+        expect(info[:product_name]).to eq("Test Product 2")
+        expect(info[:version]).to eq("2.0.0")
+
+        # Assertions for preferences
+        preferences = info[:preferences]
+        expect(preferences["enable_send_email"]).to be_true
+        expect(preferences["enable_send_sms"]).to be_true
+        expect(preferences["how_many_days_before_yahrzeit_to_send_message"]).to eq([3, 4, 5])
+      end
+    end
   end
 end
