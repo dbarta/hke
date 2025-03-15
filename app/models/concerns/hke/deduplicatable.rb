69bf98a6f7c263b7db48eb9de8175f9675df7a7f
David Barta <david@odeca.net>
2025-03-15 21:04:03 +0200

    fix loading loggable to relative

--- app/models/concerns/hke/deduplicatable.rb
+++ app/models/concerns/hke/deduplicatable.rb
@@ -1,4 +1,5 @@
-require '/Users/dbarta/Dropbox/Apps/web_apps/rails_apps/apps/hke/lib/hke/loggable.rb'
+#require '/Users/dbarta/Dropbox/Apps/web_apps/rails_apps/apps/hke/lib/hke/loggable.rb'
+require_relative '../../../lib/hke/loggable.rb'
 module Hke
   module Deduplicatable
     extend ActiveSupport::Concern
