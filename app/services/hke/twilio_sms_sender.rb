330cc49a7bebba6af99ff40ede8163a1ca1a274a
David Barta <david@odeca.net>
2025-03-18 18:28:10 +0200

    added support for twilio in production

--- app/services/hke/twilio_sms_sender.rb
+++ app/services/hke/twilio_sms_sender.rb
@@ -4,15 +4,17 @@ module Hke
   class TwilioSmsSender
     def self.send_sms(to:, message:)
       client = Twilio::REST::Client.new(
-        Rails.application.credentials.dig(:twilio, :account_sid),
-        Rails.application.credentials.dig(:twilio, :auth_token)
+        ENV['TWILIO_ACCOUNT_SID'] || Rails.application.credentials.dig(:twilio, :account_sid),
+        ENV['TWILIO_AUTH_TOKEN'] || Rails.application.credentials.dig(:twilio, :auth_token)
       )
 
       client.messages.create(
-        from: Rails.application.credentials.dig(:twilio, :phone_number),
+        from: ENV['TWILIO_PHONE_NUMBER'] || Rails.application.credentials.dig(:twilio, :phone_number),
         to: to,
         body: message
       )
     end
   end
 end
+
+
