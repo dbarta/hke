75afdfa9da71ffaff5f9078c98a030f8b0a2e50b
David Barta <david@odeca.net>
2025-04-08 21:31:21 +0300

    fix loggable loading

--- app/controllers/hke/api/v1/twilio_callback_controller.rb
+++ app/controllers/hke/api/v1/twilio_callback_controller.rb
@@ -1,10 +1,12 @@
+require_dependency 'hke/loggable'
 class Hke::Api::V1::TwilioCallbackController < Api::BaseController
-#  include Hke::Loggable
+  include Hke::Loggable
+#  Hke::Loggable
 
   def sms_status
     # Process Twilio params here
     Rails.logger.info "Twilio webhook received: #{params.to_unsafe_h}"
-    # log_info "Twilio webhook received #{params.to_unsafe_h}"
+    log_info "Twilio webhook received #{params.to_unsafe_h}"
     head :ok
   end
 end
