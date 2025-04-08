b1be1e2081e72ecbf19f725e360cbe1b43fc0b24
David Barta <david@odeca.net>
2025-04-08 21:24:32 +0300

    debug loading loggable

--- app/controllers/hke/api/v1/twilio_callback_controller.rb
+++ app/controllers/hke/api/v1/twilio_callback_controller.rb
@@ -1,11 +1,10 @@
 class Hke::Api::V1::TwilioCallbackController < Api::BaseController
-  # skip_before_action :verify_authenticity_token # since Twilio is external
-  include Hke::Loggable
+#  include Hke::Loggable
 
   def sms_status
     # Process Twilio params here
     Rails.logger.info "Twilio webhook received: #{params.to_unsafe_h}"
-    log_info "Twilio webhook received #{params.to_unsafe_h}"
+    # log_info "Twilio webhook received #{params.to_unsafe_h}"
     head :ok
   end
 end
