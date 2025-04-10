e097261192ec508e57ae1026199541d5ed8a2deb
David Barta <david@odeca.net>
2025-04-10 21:58:53 +0300

    simlify with no twilio mocks

--- spec/processors/message_processor.rb
+++ spec/processors/message_processor.rb
@@ -1,76 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Hke::MessageProcessor, type: :service do
-  let(:future_message) { create(:future_message, delivery_method: :whatsapp) }
+  let(:future_message) { create(:future_message, delivery_method: :sms, phone: ENV['TWILIO_TEST_PHONE'] || '+1234567890') }
 
   subject { described_class.new(future_message) }
 
-  before do
-    allow_any_instance_of(Hke::MessageProcessor).to receive(:send_email).and_return('email-sid')
-    allow_any_instance_of(Hke::MessageProcessor).to receive(:build_twilio_client).and_return(double('TwilioClient', messages: messages_double))
-  end
-
-  let(:messages_double) { double('Messages') }
-
   describe '#call' do
-    context 'when WhatsApp succeeds' do
-      before do
-        allow(messages_double).to receive(:create) do |params|
-          if params[:from].include?('whatsapp')
-            double('Message', sid: 'whatsapp-sid', status_code: 200)
-          else
-            double('Message', sid: 'sms-sid', status_code: 200)
-          end
-        end
-      end
+    it 'creates a sent message and deletes the future message' do
+      expect { subject.call }
+        .to change { Hke::SentMessage.count }.by(1)
+        .and change { Hke::FutureMessage.exists?(future_message.id) }.from(true).to(false)
 
-      it 'creates a sent message and deletes the future message' do
-        expect { subject.call }
-          .to change { Hke::SentMessage.count }.by(1)
-          .and change { Hke::FutureMessage.exists?(future_message.id) }.from(true).to(false)
-
-        sent_message = Hke::SentMessage.last
-        expect(sent_message.twilio_message_sid).to eq 'whatsapp-sid'
-        expect(sent_message.delivery_method).to eq 'whatsapp'
-      end
-    end
-
-    context 'when WhatsApp fails with no account, SMS fallback works' do
-      before do
-        allow(messages_double).to receive(:create) do |params|
-          if params[:from].include?('whatsapp')
-            raise Twilio::REST::RestError.new('No WhatsApp account', double(code: 63016, body: 'No WhatsApp account'))
-          else
-            double('Message', sid: 'sms-sid', status_code: 200)
-          end
-        end
-      end
-
-      it 'falls back to SMS and records fallback delivery method' do
-        expect { subject.call }
-          .to change { Hke::SentMessage.count }.by(1)
-          .and change { Hke::FutureMessage.exists?(future_message.id) }.from(true).to(false)
-
-        sent_message = Hke::SentMessage.last
-        expect(sent_message.twilio_message_sid).to eq 'sms-sid'
-        expect(sent_message.delivery_method).to eq 'sms'
-      end
-    end
-
-    context 'when Twilio raises an unexpected error' do
-      before do
-        error_response = double(status_code: 500, body: 'Twilio error body')
-        allow(messages_double).to receive(:create).and_raise(
-          Twilio::REST::RestError.new('Unexpected error', error_response)
-        )
-      end
-
-      it 'raises an error and does not create a sent message or delete the future message' do
-        expect { subject.call }.to raise_error(Twilio::REST::RestError)
-
-        expect(Hke::SentMessage.count).to eq 0
-        expect(Hke::FutureMessage.exists?(future_message.id)).to be true
-      end
+      sent_message = Hke::SentMessage.last
+      expect(sent_message.twilio_message_sid).to be_present
+      expect(sent_message.delivery_method).to eq 'sms'
     end
   end
 end
