df249128ef47054c95b3ddd94467a1bbbb83f4b7
David Barta <david@odeca.net>
2025-05-01 12:02:34 +0300

    debug 08

--- db/import_from_csv.rb
+++ db/import_from_csv.rb
@@ -2,9 +2,11 @@
 require_relative "api_seeds_executor"
 require_relative "../lib/hke/loggable"
 include Hke::Loggable
-
+puts "@@@@ 1"
 max_num_people = (ARGV.length > 0) ? ARGV[0].to_i : 1000
+puts "@@@@ 2"
 init_logging "api_import_csv"
+puts "@@@@ 3"
 log_info "@@@ Running hke/db/import_from_csv.rb file. Setting locale to :he. Importing no more than #{max_num_people} deceased people."
 executor = ApiSeedsExecutor.new(max_num_people)
 executor.process_csv(Hke::Engine.root.join("db", "deceased_2022_02_28_no_blanks.csv"))
