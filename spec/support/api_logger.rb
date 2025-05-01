0c6ddac115a895d5178ad7ff512501d4213c93e8
David Barta <david@odeca.net>
2025-05-01 11:58:23 +0300

    fix the Logger ref to Rails.logger

--- spec/support/api_logger.rb
+++ spec/support/api_logger.rb
@@ -2,7 +2,7 @@
 require "logger"
 
 def api_logger
-  @api_logger ||= Logger.new(Rails.root.join("log", "api_test.log")).tap do |log|
+  @api_logger ||= Rails.logger.new(Rails.root.join("log", "api_test.log")).tap do |log|
     log.formatter = proc do |severity, datetime, progname, msg|
       # "#{datetime.strftime("%Y-%m-%d %H:%M:%S")} #{severity}: #{msg}\n"
       "#{datetime.strftime("%H:%M:%S")} #{msg}\n"
