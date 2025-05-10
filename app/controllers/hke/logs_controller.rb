a1b7e5dca1d7d60c8200c01198690e43a6b51d46
David Barta <david@odeca.net>
2025-05-10 19:21:45 +0300

    debug

--- app/controllers/hke/logs_controller.rb
+++ app/controllers/hke/logs_controller.rb
@@ -5,25 +5,25 @@ module Hke
     include Pagy::Backend
 
     def index
-      # @event_types = Hke::Log.distinct.pluck(:event_type).compact.sort
+      @event_types = Hke::Log.distinct.pluck(:event_type).compact.sort
+
+      scope = Hke::Log.all
+
+      if params[:event_type].present?
+        scope = scope.where(event_type: params[:event_type])
+      end
+
+      if params[:start].present? && params[:end].present?
+        scope = scope.where(event_time: params[:start]..params[:end])
+      end
+
+      sort_column = %w[event_time event_type entity_type message_token ip_address error_type].include?(params[:sort]) ? params[:sort] : "event_time"
+      sort_direction = %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+
+      scope = scope.order("#{sort_column} #{sort_direction}")
 
       # scope = Hke::Log.all
-
-      # if params[:event_type].present?
-      #   scope = scope.where(event_type: params[:event_type])
-      # end
-
-      # if params[:start].present? && params[:end].present?
-      #   scope = scope.where(event_time: params[:start]..params[:end])
-      # end
-
-      # sort_column = %w[event_time event_type entity_type message_token ip_address error_type].include?(params[:sort]) ? params[:sort] : "event_time"
-      # sort_direction = %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
-
-      # scope = scope.order("#{sort_column} #{sort_direction}")
-
-      # scope = Hke::Log.all
-      # @pagy, @logs = pagy(scope, items: 100)
+      @pagy, @logs = pagy(scope, items: 100)
       @logs = Hke::Log.all
       @logs.load
       puts "@@@@@@@@@ log count: #{@logs.count}"
